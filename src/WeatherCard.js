93f0713e35386f85fc2e24d66075ab5af5a22c85
Sophia Li <sophiali124@gmail.com>
2020-03-05 18:30:11 -0800

    update state to index

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -1,20 +1,18 @@
 import React from "react";
-// import axios from "axios";
-import WeatherDetail from "./WeatherDetail";
 
-function WeatherCard() {
+const WeatherCard = ({ date, clouds, temp }) => {
   return (
     <div className="weather-card">
       <div className="card">
         <div className="card-body">
-          <h3>Weds</h3>
-          <img alt="icon"></img>
-          <p>76</p>
+          <h3>{date}</h3>
+          {/* <img alt="icon"></img> */}
+          <p>{clouds}</p>
+          <p>{temp}</p>
         </div>
       </div>
-      {/* <WeatherDetail /> */}
     </div>
   );
-}
+};
 
 export default WeatherCard;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 // import "./index.css";
 import WeatherCard from "./WeatherCard";
-import WeatherDetail from "./WeatherDetail";
+import axios from "axios";
 
 // const list = [...Array(5).keys()];
 
-const Cards = () => {
-  return (
-    <div>
-      <h2>Weather App</h2>
-      <div className="card-group">
-        <WeatherCard />
+class Cards extends Component {
+  state = {
+    weather: [],
+    temp: [],
+    clouds: [],
+    date: []
+  };
+
+  componentDidMount() {
+    let url =
+      "http://api.openweathermap.org/data/2.5/forecast?q=San+Francisco&appid=0b8748d716f8e438393856aa8448e0ba";
+    axios
+      .get(url)
+      .then(response => {
+        this.setState({
+          weather: response.data.list[0],
+          temp: response.data.list[0].main.temp,
+          clouds: response.data.list[0].weather[0].description,
+          date: response.data.list[0].dt_txt
+        });
+        console.log("day", new Date(this.state.temp).getDate());
+        console.log(this.state.temp);
+        console.log("date", this.state.date);
+        console.log("weather", this.state.weather);
+        console.log(response.data.list);
+      })
+      .catch(error => {
+        console.log("error", error);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Weather App</h2>
+        <div className="card-group">
+          <WeatherCard
+            temp={this.state.temp}
+            clouds={this.state.clouds}
+            date={this.state.date}
+          />
+        </div>
+        <div>
+          temp: {this.state.temp}
+          clouds: {this.state.clouds}
+          date: {this.state.date}
+        </div>
       </div>
-      <WeatherDetail />
-    </div>
-  );
-};
+    );
+  }
+}
 
 ReactDOM.render(<Cards />, document.getElementById("root"));
