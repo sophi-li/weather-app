ede5d5251c43e4058c88dd1e524285a7ddfa9b46
Sophia Li <sophiali124@gmail.com>
2020-03-05 21:35:54 -0800

    update from axios to fetch b/c axios is for node

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 // import "./index.css";
 import WeatherCard from "./WeatherCard";
-import axios from "axios";
+// import axios from "axios";
 
 // const list = [...Array(5).keys()];
 
 class Cards extends Component {
   state = {
+    hasErrors: false,
     weather: [],
     temp: [],
     clouds: [],
@@ -18,27 +19,14 @@ class Cards extends Component {
   componentDidMount() {
     let url =
       "http://api.openweathermap.org/data/2.5/forecast?q=San+Francisco&appid=0b8748d716f8e438393856aa8448e0ba";
-    axios
-      .get(url)
-      .then(response => {
-        this.setState({
-          weather: response.data.list[0],
-          temp: response.data.list[0].main.temp,
-          clouds: response.data.list[0].weather[0].description,
-          date: response.data.list[0].dt_txt,
-          datalist: response.data.list
-        });
-        console.log("day", new Date(this.state.date).getDate());
-        console.log("date", this.state.date);
-        console.log("weather", this.state.weather);
-        console.log(response.data.list);
-      })
-      .catch(error => {
-        console.log("error", error);
-      });
+    fetch(url)
+      .then(response => response.json())
+      .then(response => this.setState({ datalist: response.list }))
+      .catch(() => this.setState({ hasErrors: true }));
   }
 
   render() {
+    console.log("2nd", this.state.datalist);
     const arr = this.state.datalist;
     const elems = [];
     for (let i = 4; i < arr.length; i += 8) {
@@ -50,11 +38,11 @@ class Cards extends Component {
         />
       );
     }
-    // console.log("arr len", this.state.datalist);
+
     return (
       <div>
         <h2>Weather App</h2>
-        <div className="card-group">{elems}</div>
+        <div className="card-group">{elems} hi</div>
       </div>
     );
   }
