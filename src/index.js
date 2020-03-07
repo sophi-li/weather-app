ee1622548c6c29e69b5c4f946b913206a4fbeb55
Sophia Li <sophiali124@gmail.com>
2020-03-06 23:26:57 -0800

    add prettier

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 // import "./index.css";
 
-import moment from "moment";
+import moment from 'moment'
 
-import WeatherCard from "./WeatherCard";
+import WeatherCard from './WeatherCard'
 
 class Cards extends Component {
   state = {
     hasErrors: false,
     datalist: []
-  };
+  }
 
   componentDidMount() {
     let url =
-      "http://api.openweathermap.org/data/2.5/forecast?q=San+Francisco&appid=0b8748d716f8e438393856aa8448e0ba";
+      'http://api.openweathermap.org/data/2.5/forecast?q=San+Francisco&appid=0b8748d716f8e438393856aa8448e0ba'
     fetch(url)
       .then(response => response.json())
       .then(response =>
@@ -22,30 +22,30 @@ class Cards extends Component {
           datalist: response.list
         })
       )
-      .catch(() => this.setState({ hasErrors: true }));
+      .catch(() => this.setState({ hasErrors: true }))
   }
 
   render() {
     // Getting data for time equal to noon
-    const arr = this.state.datalist;
+    const arr = this.state.datalist
     let noonArray = arr.filter(function(el) {
-      el.main.temp = Math.round(((el.main.temp - 273) * 9) / 5 + 32);
-      return el.dt_txt.includes("12:00");
-    });
-    console.log("noonarray", noonArray);
+      el.main.temp = Math.round(((el.main.temp - 273) * 9) / 5 + 32)
+      return el.dt_txt.includes('12:00')
+    })
+    console.log('noonarray', noonArray)
 
     // loop through and display cards with weather data for the next 5 days
-    const elems = [];
+    const elems = []
     for (let i = 0; i < noonArray.length; i++) {
       elems.push(
         <WeatherCard
           temp={noonArray[i].main.temp}
           // Moment changes date to "Monday", "Tuesday", etc
-          date={moment(noonArray[i].dt_txt).format("dddd")}
+          date={moment(noonArray[i].dt_txt).format('dddd')}
           clouds={noonArray[i].weather[0].description}
           key={i}
         />
-      );
+      )
     }
 
     return (
@@ -53,8 +53,8 @@ class Cards extends Component {
         <h2>Weather App</h2>
         <div className="card-group">{elems}</div>
       </div>
-    );
+    )
   }
 }
 
-ReactDOM.render(<Cards />, document.getElementById("root"));
+ReactDOM.render(<Cards />, document.getElementById('root'))
